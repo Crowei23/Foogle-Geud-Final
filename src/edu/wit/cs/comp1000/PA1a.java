284491a4e1154ad0c55d652e5883ecfb9c609ec9
stell <roses4@wit.edu>
2023-09-20 21:25:55 -0400

    PA1 total number in inches from yards

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -6,6 +6,13 @@ public class PA1a {
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
-	}
+		int in = 1;
+		int ti = in*3;
+		int ft = in*12;
+		int tft = ft*2;
+		int yd = ft*3;
+		double tyd = ti+tft+yd;
+		System.out.printf("The total number of inches is %.0f",tyd);
+		}
 
 }
