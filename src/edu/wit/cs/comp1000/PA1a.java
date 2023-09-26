3afc0bcffdc0e43916e4bec8dd65db39ca3caf27
stella rose <rose4@wit.edu>
2023-09-25 20:18:33 -0400

    Stella Rose PA1- CompSci

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,18 +1,27 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
-		int in = 1;
-		int ti = in*3;
-		int ft = in*12;
-		int tft = ft*2;
-		int yd = ft*3;
-		double tyd = ti+tft+yd;
-		System.out.printf("The total number of inches is %.0f",tyd);
-		}
+		
+		Scanner input= new Scanner(System.in);
+		System.out.print("Enter number of yards: ");
+			int yard = input.nextInt();
+			int result = yard*3*12;
+		System.out.print("Enter number of feet: ");
+			int feet = input.nextInt();
+			int result1 = feet*12;
+		System.out.print("Enter number of inches: ");
+			int inches = input.nextInt();
+			int result2 = result+result1+inches;
+		System.out.println("Total number of inches: " + result2);
+		
+
+	}
 
 }
